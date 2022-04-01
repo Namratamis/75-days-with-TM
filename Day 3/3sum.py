3445aee390540d7d9a849d2a3ea791a0dab943f0
Namratamis <102582762+Namratamis@users.noreply.github.com>
2022-04-01 23:53:04 +0530

    Add files via upload

---
+++ Day 3/3sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+        for i in reversed(range(2, len(nums))):
+            if i+1 < len(nums) and nums[i] == nums[i+1]:
+                continue
+            target = -nums[i]
+            left, right = 0, i-1
+            while left < right:
+                if nums[left]+nums[right] < target:
+                    left += 1
+                elif nums[left]+nums[right] > target:
+                    right -= 1
+                else:
+                    result.append([nums[left], nums[right], nums[i]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
+        return result
